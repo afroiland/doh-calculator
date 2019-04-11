0171468eaf24af87e13593d7dd6eb8b4049ae9df
afroiland <andrew.froiland@gmail.com>
2019-04-11 15:31:59 -0500

    Add increment and decerement buttons to Counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {  }
-  render() { 
+  state = {
+    value: 0
+  }
+  render() {
     return (
-      <p>test</p>
+      <div>
+        <p>{this.state.value}</p>
+        <button>+</button>
+        <button>-</button>
+      </div>
     );
   }
 }
- 
-export default Counter;
+
+export default Counter; 
