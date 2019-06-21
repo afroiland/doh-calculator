0587c109960327a40c72cda9de6999f6b861487d
afroiland <andrew.froiland@gmail.com>
2019-06-21 15:55:38 -0500

    Starting to build out logic in calc function

--- src/functions/calc.js
+++ src/functions/calc.js
@@ -1,10 +1,22 @@
 export function calc(state) {
   console.log("state: ", state);
-
   let response = '';
+
+  let productionCapacity = (state.lands / 4) * (1 + state.reclamations);
+  console.log("productionCapacity: ", productionCapacity);
+
+  let turnsToKillOpp = state.oppLife / (state.soldiers + productionCapacity);
+  console.log("turnsToKillOpp: ", turnsToKillOpp);
+
+
   if (state.oppLife <= (state.soldiers - state.oppBlockers)) {
     response = "You should probably attack.";
-  } else{
+  // } else {
+  //   if ( ) {
+
+  //   }
+
+  } else {
     response = "Use your best judgment."
   }
 
